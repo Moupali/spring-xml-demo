0d2bc41e1875a64c9ad0aefb675fd718539fa9d2
Moupali Dutta <32608821+Moupali@users.noreply.github.com>
2019-07-17 17:01:12 +0530

    Update Movie.java
    comments added

--- src/main/java/com/stackroute/domain/Movie.java
+++ src/main/java/com/stackroute/domain/Movie.java
@@ -2,11 +2,14 @@ package com.stackroute.domain;
 
 public class Movie {
 
+    //creating object of Actor
     Actor actor;
 
+    //setter method for Actor
     public void setActor(Actor actor) {
         this.actor = actor;
     }
+    
     public void acting()
     {
         actor.drama();
